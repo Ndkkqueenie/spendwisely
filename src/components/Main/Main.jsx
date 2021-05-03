13736a55c619735b48bf279da78c9e1790389a4e
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2021-05-03 02:19:03 +0100

    snackbar to display transaction success and infocard for speech suggestion

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { ExpenseTrackerContext } from '../../context/context';
 
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
+import InfoCard from '../InfoCard';
 
 const Main = () => {
   const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
 
   return (
     <Card className={classes.root}>
-      <CardHeader className={classes.cardHeader} title="SpendWisely" subheader="Track my expenses for better financial goal" />
+      <CardHeader className={classes.cardHeader} title="SpendWisely" subheader="Track My Expenses To Improve Financial Goal" />
       <CardContent>
-        <Typography align="center" variant="h6">Total Balance #2000</Typography>
+        <Typography align="center" variant="h6">Total Balance #{balance}</Typography>
         <Typography variant="subtitle2" style={{lineHeight: '1.5em', marginTop: '20px' }}>
-          {/* InfoCard */}
-          Try saying: Add income for #500 in Category Salary for Monday ...
+          <InfoCard />
         </Typography>
         <Divider />
         <Form />
